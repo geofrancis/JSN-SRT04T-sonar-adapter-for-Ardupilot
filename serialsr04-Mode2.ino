e6feed92e068d0ce58c1951f7371fd55ea35f5cd
geofrancis <o.o.o.o@hotmail.co.uk>
2022-06-02 15:04:05 +0100

    added smoothing

--- serialsr04-Mode2.ino
+++ serialsr04-Mode2.ino
@@ -2,8 +2,6 @@
 #include <SoftwareSerial.h>
 #define I2C_SLAVE_ADDR  0x70  //Standard MaxsonarI2CXL address
 
-const int numReadings = 10;
-
 int pinRX = 10;
 int pinTX = 11;
 SoftwareSerial mySerial(pinRX, pinTX);
@@ -15,30 +13,26 @@ uint8_t Index;
 byte received;
 
 
-
+const int numReadings = 10;
 int readings[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
 int total = 0;                  // the running total
 int average = 0;                // the average
 
 
-
 void readsonar(){     
    if (mySerial.available() > 0) {
-    delay(4);
- 
+    
     // Check for packet header character 0xff
     if (mySerial.read() == 0xff) {
-      
       // Insert header into array
       data_buffer[0] = 0xff;
-      
       // Read remaining 3 characters of data and insert into array
       for (int i = 1; i < 4; i++) {
         data_buffer[i] = mySerial.read();
       }
  
-      //Compute checksum
+     //Compute checksum
       CS = data_buffer[0] + data_buffer[1] + data_buffer[2];
       // If checksum is valid compose distance from data
       if (data_buffer[3] == CS) {
@@ -72,6 +66,7 @@ void readsonar(){
 
 
 
+
 /*
  The default I2C-Address of the sensor is 0x70. 
  To perform a range measurement you must send the "Take Range Reading” command byte 0x5
@@ -82,7 +77,7 @@ void receiveEvent(int howMany) {
       received = Wire.read(); 
       if (received == 0x51)
       {
-       mySerial.write(0x55);
+ mySerial.write(0x55);
       }
     }
   }
@@ -100,11 +95,9 @@ void setup() {
  Wire.begin(I2C_SLAVE_ADDR);
  Wire.onReceive(receiveEvent); // register event
  Wire.onRequest(requestEvent);
- for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
-  }
 }
 void loop() {
   readsonar();
-  Serial.print(distance);
+     Serial.print(average);
+   Serial.println();
 }
